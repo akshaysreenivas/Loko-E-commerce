5f7a34891d75ad42e20287fc52ed1fbe690a45ad
akshay <akshaypvkeerakkal@gmail.com>
2023-09-12 14:50:11 +0530

    admin login page updated

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -12,32 +12,5 @@ cloudinary.config({
 
 module.exports=cloudinary
 
-/////////////////////////
-// Uploads an image file
-/////////////////////////
-// const uploadImage = async (req, res) => {
 
-//     // Use the uploaded file's name as the asset's public ID and 
-//     // allow overwriting the asset with new versions
-//     const options = {
-//         use_filename: true,
-//         unique_filename: false,
-//         overwrite: true,
-//     };
-
-//     try {
-//         let filePath;
-//         let fileName;
-//         if (req.file) {
-//             fileName = req.file.filename
-//             filePath = `${req.file.path}`;
-//         }
-//         // Upload the image
-//         const result = await cloudinary.uploader.upload(filePath, options);
-//         console.log(result);
-//         return result.public_id;
-//     } catch (error) {
-//         console.error(error);
-//     }
-// };
 
